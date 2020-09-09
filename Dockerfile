f35b029fd0c1c24567e1c9a93060e09e541558fa
aerrickw <aerrick@ruvos.com>
2020-09-09 16:17:11 -0400

    changed app text

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ RUN npm run build
 
 
 FROM nginx as Run
+EXPOSE 80
 COPY --from=Build /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sup hoe, yuhhhh sonnnn
+          Sup hoe, feature
         </p>
         <a
           className="App-link"
